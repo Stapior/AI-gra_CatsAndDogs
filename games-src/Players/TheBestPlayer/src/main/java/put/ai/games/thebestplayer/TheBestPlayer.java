497ae5a54050104f84ab462a7df111e53705de78
Stapior <sensaj10@gmail.com>
2019-01-14 19:10:10 +0100

    Alfa beta, jest ok

--- games-src/Players/TheBestPlayer/src/main/java/put/ai/games/thebestplayer/TheBestPlayer.java
+++ games-src/Players/TheBestPlayer/src/main/java/put/ai/games/thebestplayer/TheBestPlayer.java
@@ -7,11 +7,12 @@ package put.ai.games.thebestplayer;
 import put.ai.games.game.Board;
 import put.ai.games.game.Move;
 import put.ai.games.game.Player;
-import put.ai.games.game.moves.PlaceMove;
+
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
 
 public class TheBestPlayer extends Player {
 
@@ -19,54 +20,90 @@ public class TheBestPlayer extends Player {
     private Object object;
     private Thread thread;
     private Board board;
-    private int lastDeepth=2;
+    private int lastDeepth = 1;
 
-    private class MoveValue {
+    private float blocked(Board boardNow, int x, int y, Color player) {
+        if (boardNow.getState(x, y) == player) {
+            float value = 0;
+            if (boardNow.getState(x + 1, y + 1) == Color.EMPTY)
+                if (boardNow.getState(x + 2, y + 1) == player) {
+                    value += 0.5;
 
-        public double returnValue;
-        public Move returnMove;
-
-        public MoveValue() {
-            returnValue = 0;
+                }
+            if (boardNow.getState(x - 1, y + 1) == Color.EMPTY)
+                if (boardNow.getState(x - 2, y + 1) == player) {
+                    value += 0.5;
+                }
+            if (boardNow.getState(x + 1, y - 1) == Color.EMPTY)
+                if (boardNow.getState(x + 2, y - 1) == player) {
+                    value += 0.5;
+                }
+            if (boardNow.getState(x - 1, y - 1) == Color.EMPTY)
+                if (boardNow.getState(x - 2, y - 1) == player) {
+                    value += 0.5;
+                }
+            if (boardNow.getState(x + 1, y + 1) == Color.EMPTY)
+                if (boardNow.getState(x + 1, y + 2) == player) {
+                    value += 0.5;
+                }
+            if (boardNow.getState(x + 1, y - 1) == Color.EMPTY)
+                if (boardNow.getState(x + 1, y - 2) == player) {
+                    value += 0.5;
+                }
+            if (boardNow.getState(x - 1, y + 1) == Color.EMPTY)
+                if (boardNow.getState(x - 1, y + 2) == player) {
+                    value += 0.5;
+                }
+            if (boardNow.getState(x - 1, y - 1) == Color.EMPTY)
+                if (boardNow.getState(x - 1, y - 2) == player) {
+                    value += 0.5;
+                }
+            return value;
+        } else {
+            return 0;
         }
+    }
 
-        public MoveValue(double returnValue) {
+    private class alfaBetaMove {
+        double returnValue;
+        Move returnMove;
+
+        alfaBetaMove(double returnValue) {
             this.returnValue = returnValue;
         }
-
-        public MoveValue(double returnValue, Move returnMove) {
-            this.returnValue = returnValue;
-            this.returnMove = returnMove;
-        }
-
     }
 
 
-        public MoveValue minMax(double alpha, double beta, int maxDepth, Color player, Move move) {
+    public alfaBetaMove alfaBeta(double alpha, double beta, int maxDepth, Color player, Move move) {
 
         ArrayList<Move> moves = (ArrayList<Move>) board.getMovesFor(player);
-
         Iterator<Move> movesIterator = moves.iterator();
-        boolean isMaximizer = (player.equals(this.getColor()));
+
+        boolean isMax = (player.equals(this.getColor()));
         double value;
 
-        if (maxDepth == 0 ) {
+        if (maxDepth == 0) {
+
             Color opponent = Player.getOpponent(this.getColor());
-            value =1000 - board.getMovesFor(opponent).size();
+            value =  1000 - board.getMovesFor(opponent).size();
 
-
-            return new MoveValue(value);
+            for (int i = 0; i < board.getSize(); i++) {
+                for (int j = 0; j < board.getSize(); j++) {
+                     value += blocked(board, i, j, this.getColor())*0.1;
+                }
+            }
+            return new alfaBetaMove(value);
         }
-        MoveValue returnMove;
-        MoveValue bestMove = null;
-        if ( isMaximizer) {
-            if(moves.isEmpty()){
-                return new MoveValue(0);
+        alfaBetaMove returnMove;
+        alfaBetaMove bestMove = null;
+        if (isMax) {
+            if (moves.isEmpty()) {
+                return new alfaBetaMove(0);
             }
             while (movesIterator.hasNext()) {
                 Move currentMove = movesIterator.next();
                 board.doMove(currentMove);
-                returnMove = minMax(alpha, beta, maxDepth - 1, Player.getOpponent(player), currentMove);
+                returnMove = alfaBeta(alpha, beta, maxDepth - 1, Player.getOpponent(player), currentMove);
                 board.undoMove(currentMove);
 
                 if ((bestMove == null) || (bestMove.returnValue < returnMove.returnValue)) {
@@ -86,19 +123,18 @@ public class TheBestPlayer extends Player {
             return bestMove;
 
         } else {
-            if(moves.isEmpty()){
-                return  new MoveValue(1000);
-
+            if (moves.isEmpty()) {
+                return new alfaBetaMove(1000);
             }
             while (movesIterator.hasNext()) {
                 Move currentMove = movesIterator.next();
                 board.doMove(currentMove);
-                returnMove = minMax(alpha, beta, maxDepth - 1, Player.getOpponent(player), currentMove);
+                returnMove = alfaBeta(alpha, beta, maxDepth - 1, Player.getOpponent(player), currentMove);
                 board.undoMove(currentMove);
                 if ((bestMove == null) || (bestMove.returnValue > returnMove.returnValue)) {
                     bestMove = returnMove;
                     bestMove.returnMove = currentMove;
-            }
+                }
                 if (returnMove.returnValue < beta) {
                     beta = returnMove.returnValue;
                     bestMove = returnMove;
@@ -116,7 +152,7 @@ public class TheBestPlayer extends Player {
 
     @Override
     public String getName() {
-        return "Piotr Stachowiak 132319, Pawel Bubak 123197";
+        return "Piotr Stachowiak 132319, Pawel Bubak 132197";
     }
 
     class BestMove implements Runnable {
@@ -128,18 +164,18 @@ public class TheBestPlayer extends Player {
 
         @Override
         public void run() {
-            int deepth = lastDeepth;
+            int deepth = lastDeepth-1;
             System.out.println(deepth);
-            while(true) {
+            while (true) {
 
-                MoveValue moveValue = minMax(0, 100000000, deepth, getColor(), null);
+                alfaBetaMove moveValue = alfaBeta(0, 100000000, deepth, getColor(), null);
                 //System.out.println("zwrocono");
                 best = moveValue.returnMove;
-                lastDeepth=deepth;
-                if (deepth >100){
+                lastDeepth = deepth;
+                if (deepth > 100) {
                     break;
                 }
-                deepth+=1;
+                deepth += 1;
             }
             thread.interrupt();
 
@@ -151,7 +187,7 @@ public class TheBestPlayer extends Player {
     public Move nextMove(Board b) {
 
         long start = System.currentTimeMillis();
-        board=b.clone();
+        board = b.clone();
         thread = Thread.currentThread();
         List<Move> moves = b.getMovesFor(getColor());
         best = moves.get(0);
@@ -171,10 +207,8 @@ public class TheBestPlayer extends Player {
             } catch (InterruptedException e) {
                 return best;
             }
-
             newThread.stop();
             return best;
         }
     }
-
 }
